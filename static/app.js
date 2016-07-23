a96bb383ecedc3657c4b4bdb67ec1a8223eece59
Katsunori SUZUI <kt@7kry.net>
2016-07-23 10:23:35 +0900

    Save values to Cookie (TODO: read values)

--- static/app.js
+++ static/app.js
@@ -11,12 +11,16 @@ $(document).ready(function() {
   $('#settings_mainsize').on("change mousemove", function() {
     var v = $(this).val() * 66 + 300;
     $('#pics').css("max-width", v + 'px');
+    $.cookie('mainsize', v);
   });
   $('#settings_display input').on('change', function() {
-   if ($('input[name=settings_display]:checked', '#settings_display').val() == 'list') {
-     $('#pics').removeClass('grid');
-   } else {
-     $('#pics').addClass('grid')
-   }
+    if ($('input[name=settings_display]:checked', '#settings_display').val() == 'list') {
+      $('#pics').removeClass('grid');
+      $.cookie('grid', '0');
+    } else {
+      $('#pics').addClass('grid')
+      $.cookie('grid', '1');
+    }
+  });
 });
-});
+// vim:et:ts=2:sts=2:sw=2
