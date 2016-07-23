38778aa3af2c9ae2cde036be4d3d510e37a160d8
Katsunori SUZUI <kt@7kry.net>
2016-07-23 12:51:15 +0900

    Implement loading cookie
    Colorful Cookie Black Another

--- static/app.js
+++ static/app.js
@@ -11,10 +11,27 @@ $(document).ready(function() {
   $('#settings_mainsize').on("change mousemove", function() {
     var v = $(this).val() * 66 + 300;
     $('#pics').css("max-width", v + 'px');
-    $.cookie('mainsize', v);
+    $.cookie('mainsize', $(this).val());
   });
+  (function() {
+    var v = $.cookie('mainsize');
+    if (typeof(v) != 'undefined') {
+      $('#pics').css("max-width", (v * 66 + 300) + 'px');
+      $('#settings_mainsize').val(v);
+    }
+    v = $.cookie('grid');
+    if (v === '1') {
+      $('#pics').addClass('grid');
+      $('#settings_display-list').prop('checked', true);
+      $('#settings_display-grid').prop('checked', false);
+    } else {
+      $('#pics').removeClass('grid');
+      $('#settings_display-list').prop('checked', true);
+      $('#settings_display-grid').prop('checked', false);
+    }
+  })();
   $('#settings_display input').on('change', function() {
-    if ($('input[name=settings_display]:checked', '#settings_display').val() == 'list') {
+    if ($('input[name=settings_display]:checked', '#settings_display').val() === 'list') {
       $('#pics').removeClass('grid');
       $.cookie('grid', '0');
     } else {
